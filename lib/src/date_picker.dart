b01735bd6317eb9f1ea5b49d904bc1c3ea243e76
fsayfitdinov <fsayfitdinov@gmail.com>
2021-11-11 02:14:09 +0500

    shadow theme removed

--- lib/src/date_picker.dart
+++ lib/src/date_picker.dart
@@ -72,13 +72,10 @@ class DatePicker {
       // Check whether date format is legal or not
       if (DateTimeFormatter.isDayFormat(dateFormat)) {
         if (pickerMode == DateTimePickerMode.time) {
-          pickerMode = DateTimeFormatter.isTimeFormat(dateFormat)
-              ? DateTimePickerMode.datetime
-              : DateTimePickerMode.date;
+          pickerMode = DateTimeFormatter.isTimeFormat(dateFormat) ? DateTimePickerMode.datetime : DateTimePickerMode.date;
         }
       } else {
-        if (pickerMode == DateTimePickerMode.date ||
-            pickerMode == DateTimePickerMode.datetime) {
+        if (pickerMode == DateTimePickerMode.date || pickerMode == DateTimePickerMode.datetime) {
           pickerMode = DateTimePickerMode.time;
         }
       }
@@ -100,9 +97,8 @@ class DatePicker {
         onCancel: onCancel,
         onChange: onChange,
         onConfirm: onConfirm,
-        theme: Theme.of(context, shadowThemeOnly: true),
-        barrierLabel:
-            MaterialLocalizations.of(context).modalBarrierDismissLabel,
+        theme: Theme.of(context),
+        barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
         minuteDivider: minuteDivider,
       ),
     ).whenComplete(onClose ?? () => {});
@@ -158,14 +154,12 @@ class _DatePickerRoute<T> extends PopupRoute<T> {
   @override
   AnimationController createAnimationController() {
     assert(_animationController == null);
-    _animationController =
-        BottomSheet.createAnimationController(navigator.overlay);
+    _animationController = BottomSheet.createAnimationController(navigator.overlay);
     return _animationController;
   }
 
   @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
+  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
     double height = pickerTheme.pickerHeight;
     if (pickerTheme.title != null || pickerTheme.showTitle) {
       height += pickerTheme.titleHeight;
@@ -188,8 +182,7 @@ class _DatePickerComponent extends StatelessWidget {
   final _DatePickerRoute route;
   final double _pickerHeight;
 
-  _DatePickerComponent({Key key, @required this.route, @required pickerHeight})
-      : this._pickerHeight = pickerHeight;
+  _DatePickerComponent({Key key, @required this.route, @required pickerHeight}) : this._pickerHeight = pickerHeight;
 
   @override
   Widget build(BuildContext context) {
@@ -197,8 +190,7 @@ class _DatePickerComponent extends StatelessWidget {
     switch (route.pickerMode) {
       case DateTimePickerMode.date:
         pickerWidget = DatePickerWidget(
-          onMonthChangeStartWithFirstDate:
-              route.onMonthChangeStartWithFirstDate,
+          onMonthChangeStartWithFirstDate: route.onMonthChangeStartWithFirstDate,
           minDateTime: route.minDateTime,
           maxDateTime: route.maxDateTime,
           initialDateTime: route.initialDateTime,
@@ -245,8 +237,7 @@ class _DatePickerComponent extends StatelessWidget {
         builder: (BuildContext context, Widget child) {
           return new ClipRect(
             child: new CustomSingleChildLayout(
-              delegate: new _BottomPickerLayout(route.animation.value,
-                  contentHeight: _pickerHeight),
+              delegate: new _BottomPickerLayout(route.animation.value, contentHeight: _pickerHeight),
               child: pickerWidget,
             ),
           );
